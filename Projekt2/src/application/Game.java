d47f7a1751f74ac60cfe308d051af82833588012
Kadi Rommel <rommelkadi@gmail.com>
2015-05-08 12:06:44 +0300

    Update

--- Projekt2/src/application/Game.java
+++ Projekt2/src/application/Game.java
@@ -1,14 +1,19 @@
 package application;
+import java.util.ArrayList;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -33,6 +38,9 @@ public class Game extends Application{
 			//Mängu osa
 			BorderPane juur2= new BorderPane();
 			juur2.setId("taust");
+			//FlowPane flow = new FlowPane();
+			//juur2.setCenter(flow);
+			
 			
 			Scene scene2 = new Scene(juur,800,500);
 			scene2.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
